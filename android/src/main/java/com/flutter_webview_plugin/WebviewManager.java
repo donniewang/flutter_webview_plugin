1f0bfd983e8e9446de724c01e25736aadff73d17
donniewang <DonnieWang@qq.com>
2023-03-19 22:08:53 +0800

    bug fix

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -392,7 +392,12 @@ class WebviewManager {
 
         webView.getSettings().setSupportMultipleWindows(supportMultipleWindows);
 
-        webView.getSettings().setAppCacheEnabled(appCacheEnabled);
+//         webView.getSettings().setAppCacheEnabled(appCacheEnabled);
+        if(appCacheEnabled) {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        } else {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
 
         webView.getSettings().setAllowFileAccessFromFileURLs(allowFileURLs);
         webView.getSettings().setAllowUniversalAccessFromFileURLs(allowFileURLs);
